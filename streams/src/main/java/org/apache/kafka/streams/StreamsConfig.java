c63820d90fbf4cb849d4ee9b27906f7ddce4cfcd
Rafał Sumisławski <rafal.sumislawski@coralogix.com>
2025-11-13 14:10:03 +0100

    Port to 4.1.0: Delay followup rebalance to allow members to SyncGroup before PreparingRebalance (#5)

--- streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
+++ streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
@@ -874,6 +874,11 @@ public class StreamsConfig extends AbstractConfig {
     public static final String WINDOW_STORE_CHANGE_LOG_ADDITIONAL_RETENTION_MS_CONFIG = "windowstore.changelog.additional.retention.ms";
     private static final String WINDOW_STORE_CHANGE_LOG_ADDITIONAL_RETENTION_MS_DOC = "Added to a windows maintainMs to ensure data is not deleted from the log prematurely. Allows for clock drift. Default is 1 day";
 
+    // Coralogix modification NGSTN-1398
+    @SuppressWarnings("WeakerAccess")
+    public static final String CX_FOLLOWUP_REBALANCE_DELAY_MS_CONFIG = "cx.followup.rebalance.delay.ms";
+    public static final String CX_FOLLOWUP_REBALANCE_DELAY_MS_DOC = "";
+
     private static final String[] NON_CONFIGURABLE_CONSUMER_DEFAULT_CONFIGS =
         new String[] {ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, ConsumerConfig.GROUP_PROTOCOL_CONFIG};
     private static final String[] NON_CONFIGURABLE_CONSUMER_EOS_CONFIGS =
@@ -1272,7 +1277,13 @@ public class StreamsConfig extends AbstractConfig {
                     Type.LONG,
                     null,
                     Importance.LOW,
-                    WINDOW_SIZE_MS_DOC);
+                    WINDOW_SIZE_MS_DOC)
+            // Coralogix modification NGSTN-1398
+            .define(CX_FOLLOWUP_REBALANCE_DELAY_MS_CONFIG,
+                    Type.LONG,
+                    5 * 1000L,
+                    Importance.MEDIUM,
+                    CX_FOLLOWUP_REBALANCE_DELAY_MS_DOC);
     }
 
     // this is the list of configs for underlying clients
